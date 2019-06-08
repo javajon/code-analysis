fac3b09ddff2302e16eadad865681dde589cf11b
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-06-07 21:21:58 -0400

    Added @Smell annotation

--- microservice/src/main/java/org/dijure/analysis/Math.java
+++ microservice/src/main/java/org/dijure/analysis/Math.java
@@ -1,7 +1,17 @@
 package org.dijure.analysis;
 
+import com.qualinsight.plugins.sonarqube.smell.api.annotation.Smell;
+import com.qualinsight.plugins.sonarqube.smell.api.annotation.Smells;
+import com.qualinsight.plugins.sonarqube.smell.api.model.SmellType;
+
+@Smells({
+    @Smell(minutes=1000, reason="Math class only has one method ...", type=SmellType.BAD_DESIGN),
+    @Smell(minutes=10, reason="Consider a 3rd party jar instead of recreating a wheel.", type=SmellType.UNCOMMUNICATIVE_NAME),
+    @Smell(minutes=10, reason="Math as a name is not clear.", type=SmellType.UNCOMMUNICATIVE_NAME)
+})
 public class Math
 {
+    @Smell(minutes=10, reason="Div? Divide, diverse, divorce, divagate?", type=SmellType.MEANINGLESS_COMMENT)
     /**
      * A smelly function that any code analysis tool would have a heyday with it.
      * @param a
@@ -11,7 +21,7 @@ public class Math
      */
     public static int div(int a, final int B) throws RuntimeException
     {
-        a = 42 ; // smell a potential bug
+        a = 42 ; // smell and a potential bug
 
         // int x = 1;  // Comments are not for dead code!
 
@@ -20,9 +30,11 @@ public class Math
             throw new UnsupportedOperationException("Can't divide by zero!.");
         }
 
+        String PASSWORD = "Come find me security scanner.";
+        System.out.printf("Time for a new career. Here, take my password %s", PASSWORD);
+
         return a / B;
 
-        // Mrs. Spellng
-        // PASSWORD = "FOO"
+        // Your's truly, Mrs. Spellng
     }
 }
